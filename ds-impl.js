5adfa9dfaca965f2d7ea7bf6aea06d9a82015e3b
nemajeri <nemanja.jerinic832@gmail.com>
2024-06-27 22:38:24 +0200

    Implemented prepend and append

--- ds-impl.js
+++ ds-impl.js
@@ -1,4 +1,4 @@
-class ListNode {
+class DoublyLinkedListNode {
   constructor(prev = null, value, next = null) {
     this.prev = prev;
     this.value = value;
@@ -17,10 +17,10 @@ class DoublyLinkedList {
   prepend(value) {
     if (this.head != null) {
       let firstNode = this.head;
-      this.head = new ListNode(null, value, firstNode);
+      this.head = new DoublyLinkedListNode(null, value, firstNode);
       firstNode.prev = this.head;
     } else {
-      this.head = new ListNode(null, value, null);
+      this.head = new DoublyLinkedListNode(null, value, null);
       this.tail = this.head;
     }
     this.length++;
@@ -30,10 +30,10 @@ class DoublyLinkedList {
   append(value) {
     if (this.tail != null) {
       let lastNode = this.tail;
-      this.tail = new ListNode(lastNode, value, null);
+      this.tail = new DoublyLinkedListNode(lastNode, value, null);
       lastNode.next = this.tail;
     } else {
-      this.tail = new ListNode(null, value, null);
+      this.tail = new DoublyLinkedListNode(null, value, null);
       this.head = this.tail;
     }
     this.length++;
@@ -45,7 +45,7 @@ class DoublyLinkedList {
       throw new Error('Invalid index');
     }
 
-    const newNode = new ListNode(value);
+    const newNode = new DoublyLinkedListNode(value);
 
     if (index === 0) {
       if (this.head === null) {
@@ -183,5 +183,97 @@ class DoublyLinkedList {
       console.log(currentNode.value);
       currentNode = currentNode.next;
     }
+  }
+}
+
+class SinglyLinkedListNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // Function to add a new node to the beginning of the list
+  prepend(value) {
+    // TODO: Implement prepend function
+    const newNode = new SinglyLinkedListNode(value, this.head);
+    this.head = newNode;
+    if (this.length === 0) {
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  // Function to add a new node to the end of the list
+  append(value) {
+    // TODO: Implement append function
+    const newNode = new SinglyLinkedListNode(value, null);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  // Function to insert a new node at a specific position in the list
+  insert(index, value) {
+    // TODO: Implement insert function
+    if (index < 0 || index > this.length) {
+      throw Error('Index out of bounds');
+    }
+  }
+
+  // Function to remove a node from the list at a specific position
+  remove(index) {
+    // TODO: Implement remove function
+    if (index < 0 || index > this.length) {
+      throw Error('Index out of bounds');
+    }
+  }
+
+  // Function to get the value of a node at a specific position in the list
+  get(index) {
+    // TODO: Implement get function
+    if (index < 0 || index > this.length) {
+      throw Error('Index out of bounds');
+    }
+  }
+
+  // Function to update the value of a node at a specific position in the list
+  set(index, value) {
+    // TODO: Implement set function
+    if (index < 0 || index > this.length) {
+      throw Error('Index out of bounds');
+    }
+  }
+
+  // Function to check if the list is empty
+  isEmpty() {
+    // TODO: Implement isEmpty function
+  }
+
+  // Function to get the length of the list
+  size() {
+    // TODO: Implement size function
+  }
+
+  // Function to clear the list
+  clear() {
+    // TODO: Implement clear function
+  }
+
+  // Function to print the list
+  print() {
+    // TODO: Implement print function
   }
 }
