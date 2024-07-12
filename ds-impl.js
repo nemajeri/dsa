0744bf28bd5f654d50e5175defa1e21489638832
nemajeri <nemanja.jerinic832@gmail.com>
2024-07-12 07:22:30 +0200

    Insertion sort added, next up - searching algorithms

--- ds-impl.js
+++ ds-impl.js
@@ -472,21 +472,34 @@ const mergeHalves = (arr, startIndex, middleIndex, endIndex, temp) => {
   }
 };
 
+//Selection sort
 function selectionSortFun(arr) {
   const len = arr.length;
   for (let i = 0; i < len - 1; i++) {
-      let minIndex = i;
-      for (let j = i + 1; j < len; j++) {
-          if (arr[j] < arr[minIndex]) {
-              minIndex = j;
-          }
+    let minIndex = i;
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
       }
-      if (minIndex !== i) {
-          [arr[i], arr[minIndex]] = 
-                  [arr[minIndex], arr[i]];
-      }
+    }
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
   }
   return arr;
 }
-const arr = [64, 34, 25, 12, 22, 11, 90];
-console.log(selectionSortFun(arr)); 
+
+//Insertion sort
+function insertionSortFun(arr) {
+  const len = arr.length;
+  for (let i = 1; i < len; i++) {
+    let current = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
