f2c770c3d52564737de6ba0dd0b59d92b95cad4a
nemajeri <nemanja.jerinic832@gmail.com>
2024-07-17 07:51:22 +0200

    Graph implementation

--- ds-impl.js
+++ ds-impl.js
@@ -503,3 +503,28 @@ function insertionSortFun(arr) {
   }
   return arr;
 }
+
+//Graph theory
+class Graph {
+  constructor() {
+    this.list = {};
+  }
+
+  addNode(node) {
+    this.list[node] = [];
+  }
+
+  addEdge(fromNode, toNode) {
+    this.list[fromNode].push(toNode);
+    this.list[toNode].push(fromNode);
+  }
+
+  removeEdge(fromNode, toNode) {
+    this.list[fromNode] = this.list[fromNode].filter((node) => node !== toNode);
+    this.list[toNode] = this.list[toNode].filter((node) => node !== fromNode);
+  }
+
+  isEdge(fromNode, toNode) {
+    return this.list[fromNode].includes(toNode);
+  }
+}
